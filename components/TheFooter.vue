8eb9ca6c7df27bcffb49f11653881f095eb0575c
Gustavo <gustavobenicio@gmail.com>
2023-09-25 21:30:38 +0100

    change whatsaoologo and footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-zinc-200 ">
+  <footer class="bg-zinc-200 dark:bg-gray-800 ">
     <div class="container mx-auto px-8">
       <div class="w-full flex flex-col md:flex-row py-6">
         <div class="flex-1 mb-6">
@@ -51,53 +51,53 @@
             </li>
           </ul>
         </div> -->
-        <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Social</p>
+        <div class="flex-1 ">
+          <p class="uppercase text-gray-500 md:mb-6 dark:!text-gray-200 ">Social</p>
           <ul class="list-reset mb-6">
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-sky-500"
+                class="no-underline hover:underline text-gray-800 dark:!text-gray-400  hover:text-sky-500"
                 >Facebook</a
               >
             </li>
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-sky-500"
+                class="no-underline hover:underline text-gray-800 dark:!text-gray-400  hover:text-sky-500"
                 >Linkedin</a
               >
             </li>
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-sky-500"
+                class="no-underline hover:underline text-gray-800 dark:!text-gray-400 hover:text-sky-500"
                 >X (Twitter)</a
               >
             </li>
           </ul>
         </div>
         <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Companhia</p>
+          <p class="uppercase text-gray-500 dark:!text-gray-200  md:mb-6">Companhia</p>
           <ul class="list-reset mb-6">
             <!-- <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-sky-500"
+                class="no-underline hover:underline text-gray-800 dark:!text-gray-200  hover:text-sky-500"
                 >Official Blog</a
               >
             </li> -->
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#AboutUs"
-                class="no-underline hover:underline text-gray-800 hover:text-sky-500"
+                class="no-underline hover:underline text-gray-800 dark:!text-gray-400  hover:text-sky-500"
                 >Sobre nós</a
               >
             </li>
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#ContactUs"
-                class="no-underline hover:underline text-gray-800 hover:text-sky-500"
+                class="no-underline hover:underline text-gray-800 dark:!text-gray-400  hover:text-sky-500"
                 >Contactos</a
               >
             </li>

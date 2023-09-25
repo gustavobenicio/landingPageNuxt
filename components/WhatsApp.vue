51a6441cd65252820662c9f5a044c70aa4400a88
Gustavo <gustavobenicio@gmail.com>
2023-09-25 21:45:53 +0100

    fixing whatsapplogo

--- components/WhatsApp.vue
+++ components/WhatsApp.vue
@@ -5,7 +5,7 @@
                 <p class="ctc-analytics ctc_cta ctc_cta_stick ht-ctc-cta  ht-ctc-cta-hover "
                     style="padding: 0px 16px; line-height: 1.6; font-size: 15px; background-color: rgb(37, 211, 102); color: rgb(255, 255, 255); border-radius: 10px; margin: 0px 10px; order: 0; display: none;">
                     Contacte-nos</p>
-                <img src="img/whats-logo.svg" />
+                <img src="/img/whats-logo.svg" />
 
         </div>
     </div>

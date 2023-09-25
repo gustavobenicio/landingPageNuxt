43c6a6a77add959e75242d9b6ce1e6de1bd5b685
Gustavo <gustavobenicio@gmail.com>
2023-09-25 21:42:19 +0100

    change whatsaoologo and footer

--- components/WhatsApp.vue
+++ components/WhatsApp.vue
@@ -6,6 +6,7 @@
                     style="padding: 0px 16px; line-height: 1.6; font-size: 15px; background-color: rgb(37, 211, 102); color: rgb(255, 255, 255); border-radius: 10px; margin: 0px 10px; order: 0; display: none;">
                     Contacte-nos</p>
                 <img src="img/whats-logo.svg" />
+
         </div>
     </div>
 </div></template>
